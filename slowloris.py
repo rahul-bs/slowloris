7f6b734219ff0409e524614945a0d25ed63ea34f
Rahul Jaisinghani <51262163+rahul-bs@users.noreply.github.com>
2022-09-05 08:35:26 -0700

    Update slowloris.py

--- slowloris.py
+++ slowloris.py
@@ -2,7 +2,7 @@ sample_job_1:
 	tags:
 	 - ci
 	script:
-	 - export BROWSERSTACK_USERNAME=sachinm_mSzRqL && export BROWSERSTACK_ACCESS_KEY=A2mEqKgsxzc9z2NVyT8s
+	 - export BROWSERSTACK=sachinm_mSzRqL && export BROWSERSTACK=A2mEqKgsxzc9z2NVyT8s
 	 - npm install
 	 - npm run test
 	after_script:
